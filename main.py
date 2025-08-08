86a70d49f473c7b5f5ba118687054023837698cc
pndaRN <markbwilliamsjr@gmail.com>
2025-08-08 10:22:28 -0500

    going through the textual tutorial to learn the system

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
 def main():
     print("Hello from silentmemoir!")
 
+class StopwatchApp(App):
+    
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
 
 if __name__ == "__main__":
-    main()
+    app = StopwatchApp()
+    app.run()
