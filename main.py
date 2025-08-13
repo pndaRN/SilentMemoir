bcc514ca0a02aa5819e8e712c1e1877db95cfcf4
pndaRN <markbwilliamsjr@gmail.com>
2025-08-13 15:37:21 -0500

    I have timers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from time import monotonic
+
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
+from textual.reactive import reactive
 from textual.widgets import Button, Digits, Footer, Header
 
 def main():
@@ -8,17 +11,39 @@ def main():
 class TimeDisplay (Digits):
     """A widget to display elapsed time."""
 
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
+
+    def on_mount(self) -> None:
+        self.set_interval(1 / 60, self.update_time)
+
+    def update_time(self) -> None:
+        self.time = monotonic() - self.start_time
+
+    def watch_time(self, time: float) -> None:
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
+
 class Stopwatch(HorizontalGroup):
+
+    def on_botton_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "start":
+            self.add_class("started")
+        elif event.button.id == "stop":
+            self.remove_class("started")
 
     def compose(self) -> ComposeResult:
 
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
         yield Button("Reset", id="reset") 
-        yield TimeDisplay("00:00:00.00") 
+        yield TimeDisplay("") 
+
 
 class StopwatchApp(App):
     
+    CSS_PATH = "stopwatch.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
