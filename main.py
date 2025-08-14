dd82efa844242578ff62ba79cfa3c0928786d630
pndaRN <markbwilliamsjr@gmail.com>
2025-08-13 21:06:05 -0500

    finished textual tutorial

--- main.py
+++ main.py
@@ -5,57 +5,102 @@ from textual.containers import HorizontalGroup, VerticalScroll
 from textual.reactive import reactive
 from textual.widgets import Button, Digits, Footer, Header
 
-def main():
-    print("Hello from silentmemoir!")
 
-class TimeDisplay (Digits):
+class TimeDisplay(Digits):
     """A widget to display elapsed time."""
 
     start_time = reactive(monotonic)
     time = reactive(0.0)
+    total = reactive(0.0)
 
     def on_mount(self) -> None:
-        self.set_interval(1 / 60, self.update_time)
+        """Event handler called when widget is added to the app."""
+        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
 
     def update_time(self) -> None:
-        self.time = monotonic() - self.start_time
+        """Method to update time to current."""
+        self.time = self.total + (monotonic() - self.start_time)
 
     def watch_time(self, time: float) -> None:
+        """Called when the time attribute changes."""
         minutes, seconds = divmod(time, 60)
         hours, minutes = divmod(minutes, 60)
         self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
 
-class Stopwatch(HorizontalGroup):
+    def start(self) -> None:
+        """Method to start (or resume) time updating."""
+        self.start_time = monotonic()
+        self.update_timer.resume()
 
-    def on_botton_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "start":
+    def stop(self) -> None:
+        """Method to stop the time display updating."""
+        self.update_timer.pause()
+        self.total += monotonic() - self.start_time
+        self.time = self.total
+
+    def reset(self) -> None:
+        """Method to reset the time display to zero."""
+        self.total = 0
+        self.time = 0
+
+
+class Stopwatch(HorizontalGroup):
+    """A stopwatch widget."""
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        button_id = event.button.id
+        time_display = self.query_one(TimeDisplay)
+        if button_id == "start":
+            time_display.start()
             self.add_class("started")
-        elif event.button.id == "stop":
+        elif button_id == "stop":
+            time_display.stop()
             self.remove_class("started")
+        elif button_id == "reset":
+            time_display.reset()
 
     def compose(self) -> ComposeResult:
-
+        """Create child widgets of a stopwatch."""
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
-        yield Button("Reset", id="reset") 
-        yield TimeDisplay("") 
+        yield Button("Reset", id="reset")
+        yield TimeDisplay()
 
 
 class StopwatchApp(App):
-    
+    """A Textual app to manage stopwatches."""
+
     CSS_PATH = "stopwatch.tcss"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("a", "add_stopwatch", "Add"),
+        ("r", "remove_stopwatch", "Remove")
+
+    ]
 
     def compose(self) -> ComposeResult:
-        
+        """Called to add widgets to the app."""
         yield Header()
         yield Footer()
-        yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch())
+        yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch(), id="timers")
+
+    def action_add_stopwatch(self) -> None:
+        new_stopwatch = Stopwatch()
+        self.query_one("#timers").mount(new_stopwatch)
+        new_stopwatch.scroll_visible()
+
+    def action_remove_stopwatch(self) -> None:
+        timers = self.query("Stopwatch")
+        if timers:
+            timers.last().remove()
 
     def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
+
 
 if __name__ == "__main__":
     app = StopwatchApp()
